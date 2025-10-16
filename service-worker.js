cc4b09e77db4a98d345cb1fc96c6f8b2e89f88c8
drpetrov <petrovpmv29@gmail.com>
2025-10-16 15:08:46 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,25 @@
 const CACHE_NAME = 'dr-petrov-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './logo.png'
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon-180.png',
+  '/logo.png',
+  '/Manrope-Regular.woff2',
+  '/Manrope-Bold.woff2'
 ];
 
-// Установка Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Кэш открыт');
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Активация Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Удаление старого кэша:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Обработка запросов
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Возвращаем закэшированный ответ или загружаем из сети
-        return response || fetch(event.request);
-      })
+      .then((response) => response || fetch(event.request))
   );
 });
